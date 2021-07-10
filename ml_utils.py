5d8f327915908f75198cd1afff10fad554a3cf53
CWmonika <monika.korrapati@carwale.com>
2021-07-10 16:52:33 +0530

    Assignment Tasks

--- ml_utils.py
+++ ml_utils.py
@@ -2,9 +2,19 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 
 # define a Gaussain NB classifier
 clf = GaussianNB()
+# other classifiers
+clf2 = DecisionTreeClassifier()
+clf3 = KNeighborsClassifier(n_neighbors=7, metric='euclidean')
+clf4 = make_pipeline(StandardScaler(),
+                    SGDClassifier(max_iter=1000, tol=1e-3))
 
 # define the class encodings and reverse encodings
 classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
@@ -18,16 +28,27 @@ def load_model():
     # do the test-train split and train the model
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     clf.fit(X_train, y_train)
+    clf2.fit(X_train, y_train)
+    clf3.fit(X_train, y_train)
+    clf4.fit(X_train, y_train)
 
     # calculate the print the accuracy score
     acc = accuracy_score(y_test, clf.predict(X_test))
-    print(f"Model trained with accuracy: {round(acc, 3)}")
+    acc2 = accuracy_score(y_test, clf2.predict(X_test))
+    acc3 = accuracy_score(y_test, clf3.predict(X_test))
+    acc4 = accuracy_score(y_test, clf4.predict(X_test))
 
+    print(f"Model trained with accuracy: {round(acc, 3)}")
+    print(f"Model2 trained with accuracy: {round(acc2, 3)}")
+    print(f"Model3 trained with accuracy: {round(acc3, 3)}")
+    print(f"Model4 trained with accuracy: {round(acc4, 3)}")
 
 # function to predict the flower using the model
 def predict(query_data):
     x = list(query_data.dict().values())
-    prediction = clf.predict([x])[0]
+    # Observation: Out of all 4 classifiers, KNN performed well on all relaods compared to inconsistent accuaracy from other models
+    # So using KNN to predict
+    prediction = clf3.predict([x])[0]
     print(f"Model prediction: {classes[prediction]}")
     return classes[prediction]
 
@@ -38,4 +59,4 @@ def retrain(data):
     y = [r_classes[d.flower_class] for d in data]
 
     # fit the classifier again based on the new data obtained
-    clf.fit(X, y)
+    clf3.fit(X, y)
--- test_app.py
+++ test_app.py
@@ -23,4 +23,50 @@ def test_pred_virginica():
         response = client.post("/predict_flower", json=payload)
         # asserting the correct response is received
         assert response.status_code == 200
-        assert response.json() == {"flower_class": "Iris Virginica"}
+        assert response.json()["flower_class"] == "Iris Virginica"
+
+
+# test to check if Iris Setosa is classified correctly
+def test_pred_Setosa():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 4.9,
+        "sepal_width": 3.0,
+        "petal_length": 1.4,
+        "petal_width": 0.2,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json()["flower_class"] == "Iris Setosa"
+
+# test to check if Iris Versicolour is classified correctly
+def test_pred_Versicolour():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 5.8,
+        "sepal_width": 2.7,
+        "petal_length": 4.1,
+        "petal_width": 1.0,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json()["flower_class"] == "Iris Versicolour"
+
+# test to check the correct functioning of the /feedback_loop route
+def test_feedback_loop():
+    payload = [{
+        "sepal_length": 3.0,
+        "sepal_width": 5.0,
+        "petal_length": 3.2,
+        "petal_width": 4.4,
+        "flower_class": "Iris Virginica"
+    }]
+    with TestClient(app) as client:
+        response = client.post("/feedback_loop", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"detail": "Feedback loop successful"} 
